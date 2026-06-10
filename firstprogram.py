17a6090314902a84e1cca06d903ee06cc69db3bb
Glen Lobo0 <Glenlobo2007@gmail.com>
2026-06-10 19:27:44 +0530

    Initial commit

---
+++ firstprogram.py
@@ -0,0 +1,5 @@
+first = int(input("enter first:"))
+
+second = int(input("enter second:"))
+jls_extract_var = "sum ="
+print(jls_extract_var, first + second)
---
+++ lecture2.py
@@ -0,0 +1,9 @@
+str = int(input("Enter your marks:"))
+if str >= 90:
+    print("grade = A")
+elif str >= 80:
+    print("grade = B")
+elif str >= 70:
+    print("grade = C")
+else:
+    print("grade = D")
